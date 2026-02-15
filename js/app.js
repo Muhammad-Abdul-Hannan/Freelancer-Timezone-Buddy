09ceff3342893039565b73a0358618642ff1e410
Muhammad Abdul Hannan <hannanryk2@gmail.com>
2026-02-15 17:02:48 +0500

    Completed App (UI + Features)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+import Lenis from "https://cdn.jsdelivr.net/npm/@studio-freight/lenis@1.0.42/+esm";
+
+const lenis = new Lenis({
+    duration: 1.1,
+    smoothWheel: true,
+    smoothTouch: false
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.addEventListener("click", (e) => {
+    const link = e.target.closest('a[href^="#"]');
+    if (!link) return;
+
+    const hash = link.getAttribute("href");
+    if (!hash || hash === "#") return;
+
+    const target = document.querySelector(hash);
+    if (!target) return;
+
+    e.preventDefault();
+
+    // Lenis smooth scroll to section
+    lenis.scrollTo(target, {
+        offset: 0,        // change later if you add sticky header
+        duration: 1.1     // tweak for faster/slower anchor scroll
+    });
+});
+
+
+
 import { savemeetingdialog, closeDialog, renderConversion, renderMeetings } from "./ui.js";
 import { populateTimeZones, convertTime } from "./timezone.js";
 import { formatTimeRemaining } from "./utils.js";
@@ -67,6 +103,7 @@ function meetingSchedule() {
     let meetingData = new MeetingData(lastConversion, title, clientName, platform, description, fromTimeZone, toTimeZone);
 
     addMeeting(meetingData);
+    meetingcard.style.display = "grid";
 
     const data = loadData();
 
@@ -101,9 +138,51 @@ export function deleteMeeting() {
     allData.meetings = allData.meetings.filter(m => m.id !== clickedId);
     saveData(allData);
     const data = loadData();
+    if (data.meetings.length === 0) {
+        meetingcard.style.display = "none";
+    }
     renderMeetings(data.meetings);
 }
 
 
 const data = loadData();
-renderMeetings(data.meetings);
+const meetingcard = document.getElementById("meeting-cards");
+if (data.meetings.length === 0) {
+    meetingcard.style.display = "none";
+}
+else {
+    meetingcard.style.display = "grid";
+    renderMeetings(data.meetings);
+}
+
+
+
+/* ================= THEME ================= */
+
+const toggle =
+    document.getElementById("themeToggle");
+toggle.addEventListener("click", () => {
+    console.log("Yes");
+    const root = document.documentElement;
+
+    const next =
+        root.dataset.theme === "dark"
+            ? "light"
+            : "dark";
+
+    root.dataset.theme = next;
+    localStorage.setItem("theme", next);
+});
+
+const savedTheme =
+    localStorage.getItem("theme");
+
+if (savedTheme) {
+    document.documentElement.dataset.theme =
+        savedTheme;
+} else if (
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+    document.documentElement.dataset.theme =
+        "dark";
+}
